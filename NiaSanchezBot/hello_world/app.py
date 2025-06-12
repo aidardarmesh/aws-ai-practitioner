8f669dfbfd61a0748f58cfcf5d795619629d1248
Aidar Darmesh <darmesh.aidar@gmail.com>
2025-06-12 14:08:14 +0500

    Used code from an example

--- NiaSanchezBot/hello_world/app.py
+++ NiaSanchezBot/hello_world/app.py
@@ -84,39 +84,7 @@ chain = LLMChain(
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-    # Create a new event loop if running in Lambda environment
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    result = loop.run_until_complete(main(event, context))
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": result,
-            }
-        ),
-    }
+    return asyncio.get_event_loop().run_until_complete(main(event, context))
 
 
 async def ask(update: Update, context: ContextTypes.DEFAULT_TYPE):
