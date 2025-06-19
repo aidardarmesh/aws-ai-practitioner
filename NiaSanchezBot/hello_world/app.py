d6a347945ff759ccc03f2ac829880f9b4671f64c
Aidar Darmesh <darmesh.aidar@gmail.com>
2025-06-19 14:08:33 +0500

    Calling Bedrock again

--- NiaSanchezBot/hello_world/app.py
+++ NiaSanchezBot/hello_world/app.py
@@ -94,8 +94,8 @@ async def ask(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.send_message(chat_id=update.message.chat.id, text="Задавайте вопрос, используя команду /ask <ваш вопрос>.")
         return
 
-    # response = chain.run(question)
-    response = "hello man"
+    response = chain.run(question)
+    # response = "hello man"
     await context.bot.send_message(chat_id=update.message.chat.id, text=response)
 
 
